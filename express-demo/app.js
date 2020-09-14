4cb7985616ae895d681b2cda4d76edb2370bee72
Sophia Upshaw <sophiaupshaw@Sophias-Air.attlocal.net>
2020-09-14 14:02:49 -0400

    REST API finished

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,5 +1,6 @@
+// loading neccessary modules
+const Joi = require('joi'); // returns a class (capitalize first letter)
 const express = require('express');
-
 const app = express(); // options with app -> get, post, put, delete
 
 app.use(express.json()); 
@@ -20,15 +21,64 @@ app.get('/api/courses', (req, res) => {
 
 // add a new course
 app.post('/api/courses', (req, res) => {
+    // validation logic can be replaced with joi npm package
+    const { error } = validateCourse(req.body); // = result.error
+    if (error) return res.status(400).send(error.details[0].message);
+
     const course = {
         id: courses.length + 1,
-        name: req.body.name
+        name: req.body.name // we need input validation, what if user doesn't provide a name?
     };
     courses.push(course);
     res.send(course);
 })
 
-// endpoint: /api/courses/1=
+// update a course
+app.put('/api/courses/:id', (req, res) => {
+    // look up course with given id
+    // if it doesn't exist, return 404, not found
+    course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send('The course with the given ID was not found.'); // return status 404 - object not found
+    }
+    // Validate
+    // If invalid, return 400 - Bad request
+    // using object destructuring because we only need error from result
+    const { error } = validateCourse(req.body); // = result.error
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Update course
+    course.name = req.body.name;
+
+    // Return the updated course
+    res.send(course);
+})
+
+// delete a course
+app.delete('/api/courses/:id', (res, req) => {
+    // Look up course
+    // Doesn't exist? return 404
+    course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.'); // return status 404 - object not found
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // Return the same course
+    res.send(course);
+});
+
+// function for validating course
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    return Joi.validate(course, schema);
+}
+
+// endpoint: /api/courses/1
 app.get('/api/courses/:id', (req, res) => {
     course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('The course with the given ID was not found.'); // return status 404 - object not found
